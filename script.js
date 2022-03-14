1467f6a07a466ed4434e2687fdb2bd4147ea5bd1
Antonis <marasantonis@gmail.com>
2022-03-14 20:04:28 +0200

    validation with constraint API

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
-const inputs = document.querySelectorAll("input");
-const warning = document.querySelector("input + p");
-const warnings = document.querySelectorAll("input + p");
+const form = document.querySelector("form");
+const userName = document.getElementById("firstname");
+const userLastName = document.getElementById("lastname");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("pswd-confirm");
 
-warnings.forEach((warning) => (warning.style.visibility = "hidden"));
-
-inputs.forEach((input) => {
-  input.addEventListener("focus", () => {
-    visible(input.id);
-  });
-});
-
-inputs.forEach((input) => {
-  input.addEventListener("blur", () => hidden(input.id));
-});
-
-inputs.forEach((input) => {
-  input.addEventListener("input", () => validation(input.value, input.id));
-});
-
-// functions
-
-function visible(value) {
-  document.querySelector(`#${value} + p`).style.visibility = "visible";
-}
-
-function hidden(value) {
-  document.querySelector(`#${value} + p`).style.visibility = "hidden";
-}
-
-function validation(value, id) {
-  console.log(typeof value);
-  let numbers = /[0-9]/g;
-  if (value === "") {
-    document.querySelector(`#${id} + p`).textContent =
-      "*Please enter a valid name";
+userName.addEventListener("input", () => {
+  if (userName.validity.valid) {
+    userName.classList.remove("error");
+    userName.nextElementSibling.style.visibility = "hidden";
   }
+});
 
-  if (value.match(numbers)) {
-    return (document.querySelector(`#${id} + p`).textContent =
-      "*Name should contain letters only");
+form.addEventListener("submit", (e) => {
+  if (!userName.validity.valid) {
+    userName.classList.add("error");
+    showErrorName();
+    userName.nextElementSibling.style.visibility = "visible";
+    e.preventDefault();
+  }
+});
+
+function showErrorName() {
+  if (userName.validity.valueMissing) {
+    userName.nextElementSibling.textContent = "*Please enter your first name";
+  } else if (userName.validity.patternMismatch) {
+    userName.nextElementSibling.textContent =
+      "Your name should only contain letters";
+  } else if (userName.validity.tooShort) {
+    userName.nextElementSibling.textContent =
+      "*Your name should be min 3 and max 12 characters!";
   }
 }
+// if (!userLastName.validity.valid) {
+//   userLastName.classList.add("error");
+//   userLastName.nextElementSibling.style.visibility = "visible";
+//   e.preventDefault();
+// }
+// if (!phone.validity.valid) {
+//   phone.classList.add("error");
+//   phone.nextElementSibling.style.visibility = "visible";
+//   e.preventDefault();
+// }
+// if (!email.validity.valid) {
+//   email.classList.add("error");
+//   email.nextElementSibling.style.visibility = "visible";
+//   e.preventDefault();
+// }
+// if (!password.validity.valid) {
+//   password.classList.add("error");
+//   password.nextElementSibling.style.visibility = "visible";
+//   e.preventDefault();
+// }
